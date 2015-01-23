96316aa34508cc00e2e5b4e8624caec7dd7469ed
rkn218 <rkn218@lehigh.edu>
2015-01-23 22:37:28 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+// Rami Nasrallah
+// Welcome Class java program
+//
+//  First compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args)  {
+        
+//  Print the statement
+        System.out.println(" -----------------" );
+        System.out.println(" | Welcome Class |" );
+        System.out.println(" -----------------" );
+        System.out.println("  ^  ^  ^  ^  ^  ^" );
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\" );
+        System.out.println("<-r--k--n--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v"  );
+        System.out.println("This winter break I went back home to Cairo, Egypt" );
+        
+    }
+}
