43945bf937e77ead5a3522b8e1358c1192f31aa9
rkn218 <rkn218@lehigh.edu>
2015-02-18 02:36:00 +0000

    updated Banking

--- hw04/Banking.java
+++ hw04/Banking.java
@@ -9,54 +9,51 @@ public class Banking{
     // main method required for every Java program
    	public static void main(String[] args) {
    	int num;
-   	int balance;
-   	int number;
    	String response;
-   	String deposit;
-   	String withdraw;
-   	String balance;
+   	int deposit;
+   	int withdraw;
+   	int balance;
    	Scanner myScanner;
     myScanner = new Scanner( System.in );
    	 
-   	Random r = new Random();
     int Low = 1000;
     int High = 5000;
-    int number = r.nextInt(High-Low) + Low;
+    int number = (int)(Math.random()*(High-Low) + Low);
    	System.out.println("Would you like to view your balance, withdraw or deposit in your balance?");
-   	System.out.println("Answer with 'view', 'withdraw' or 'balance': ");
+   	System.out.println("Answer with 'view', 'withdraw' or 'deposit': ");
    	
    	response = myScanner.next();
    	
-   	switch (response):
+   	switch (response)
    	  {
-         case 'view' :
+         case "view" :
             System.out.println("Balance = $" +number); 
             break;
-         case 'withdraw' :
-             System.out.prinln("Enter how much you would like to withdraw: ");
-             num = myScanner.next();
+         case "withdraw" :
+             System.out.println("Enter how much you would like to withdraw: ");
+             num = myScanner.nextInt();
              if (num>0){
                  balance = number-num;
                  System.out.println("The resultant balance = $" +balance);
-                 else num<0||number-num<0
-                 System,out,println("The number you entered is invalid.(Either not a positive number or not enough money in account");
              }
+                 else if(num<0||number-num<0)
+                 System.out.println("The number you entered is invalid.(Either not a positive number or not enough money in account");
              break;
-         case 'deposit' :
+         case "deposit" :
             System.out.println("Enter how much you would like to deposit: ");
-            num = myScanner.next();
+            num = myScanner.nextInt();
                 if (num<0){
                     System.out.println("Number must be positive");
-                    else num>0
+                }
+                    else if(num>0) {
                     balance = number+num;
                     System.out.println("The resultant balance = $" +balance);
-                }
+                    }
             break;
          
          default :
             System.out.println("Invalid Answer");
       }
-   	
    	
    	}
     
