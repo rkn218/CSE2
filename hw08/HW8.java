2dce9165c6106e8a2b77a2c6360f0e09f8e09398
rkn218 <rkn218@lehigh.edu>
2015-03-23 12:14:17 +0000

    updated HW8

--- hw08/HW8.java
+++ hw08/HW8.java
@@ -67,23 +67,58 @@ public class HW8{
                     return input;
                 }
                 else{
+                    System.out.println("Yea right LOSER!");
                     System.exit(1);
                 }
                 return input; 
             }
-            public static String getInput(Scanner key, String key1, int hat){
-                hat=10;
+            public static String getInput(Scanner key, String key1, int trial){
                 String input = key.next();
-                if (input.equals("A")||input.equals("a")||input.equals("E")||input.equals("e")){
-                    return input;
-                }
-                else{
-                    System.exit(1);
+                while (trial>0) {
+                    if (input.equals("A")||input.equals("a")){
+                        int randomNum = 0 + (int)(Math.random()*2);
+                        if (randomNum==1){
+                            System.out.println("Critical Hit!");
+                            trial--;
+                            input=key.next();
+                        }
+                        else {
+                            System.out.println("Gosh! How can you miss it!");
+                            randomNum = 0 + (int)(Math.random()*1);
+                            input=key.next();
+                        }
+                    }
+                    else if (input.equals("E")||input.equals("e")){
+                    int randomNum = 1 + (int)(Math.random()*11);
+                        if (randomNum==10){
+                            System.out.println("Escaped Successfully!");    
+                            System.exit(1);
+                        }
+                        else {
+                            System.out.println("You failed to escape");
+                            System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'e' to Escape, ANYTHING else is to YIELD");
+                            input = key.next();
+                        }
+                    }
+                    else{
+                        System.out.println("Executed by the GIANT! Game Over!");
+                        System.exit(1);
+                    }
                 }
                 return input;
             }
             public static String getInput(Scanner key2){
                 String scan = key2.next();
+                switch (scan){
+                    case "1" :
+                        System.out.println("You found a million dollars!");
+                        break;
+                    case "2" :
+                        System.out.println("The treasure box is filled with jewels!");
+                        break;
+                    case "3" :    
+                        System.out.println("You found a dragon in the treasure box!");
+                } 
                 return scan;
             }
 }
